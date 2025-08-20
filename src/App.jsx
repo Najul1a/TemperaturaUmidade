5847fc66cb13f9c741141725512d743947716d2e
Ana Geremias <anageremiias@gmail.com>
2025-08-20 11:44:46 -0300

    fix: update MQTT topic for temperature data source

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   // Configurações do MQTT
   const brokerHost = "broker.hivemq.com";
   const brokerPort = 8884;
-  const topic = "profcastello/temperatura";
+  const topic = "Najulia/temperatura";
 
   // Hook customizado para gerenciar MQTT
   const { temperature, humidity, isConnected, lastUpdate, connectionAttempts } =
